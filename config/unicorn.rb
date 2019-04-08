b0454892125d98d1e524d9b87e2bc90cf6fcd1ab
yoshimasa1125 <yoshimasahosoi1125@gmail.com>
2019-04-08 19:52:39 +0900

    Unicorn.rb 元に戻した

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 app_path = File.expand_path('../../', __FILE__)
 
-# worker_processes 1
+worker_processes 1
 
 working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
@@ -9,7 +9,7 @@ stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 listen 3000
-timeout 3000
+timeout 60
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
