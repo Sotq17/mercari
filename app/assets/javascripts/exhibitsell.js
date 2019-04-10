b0692929cddabe1b2697d1d1d1192abad4c432e5
nakamotoyuto <tenisu0303@icloud.com>
2019-04-10 21:54:31 +0900

    商品出品ページ変更

--- app/assets/javascripts/exhibitsell.js
+++ app/assets/javascripts/exhibitsell.js
@@ -1,15 +1,15 @@
 $(function(){
   $(".exhibit__price__box__right__input").on('keyup', function(e){
-  	e.preventDefault();
-  	var input = $(".exhibit__price__box__right__input").val();
-  	if( 300 <= input && input <= 9999999) {
-      var num  = Math.floor(input / 10);
-      var num2 = (input - num);
-      $(".exhibit__price__com__right").text(num);
-      $(".exhibit__price__com__right__2").text(num2);
-    } else{
-      $(".exhibit__price__com__right").text("-");
-      $(".exhibit__price__com__right__2").text("-");
-    }
+    e.preventDefault();
+      var input = $(".exhibit__price__box__right__input").val();
+  	  if( 300 <= input && input <= 9999999) {
+        var num  = Math.floor(input / 10);
+        var num2 = (input - num);
+        $(".exhibit__price__com__right").text(num);
+        $(".exhibit__price__com__right__2").text(num2);
+      } else{
+        $(".exhibit__price__com__right").text("-");
+        $(".exhibit__price__com__right__2").text("-");
+      }
    });
 });
