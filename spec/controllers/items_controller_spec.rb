ddbad54c62eadebddbf57ad406d5601ecf6e097c
Sota Takahashi <sotq17@gmail.com>
2019-04-12 12:03:56 +0900

    トップページ修正　検索機能実装
    トップページ修正と
    検索機能を実装しました！

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe ItemsController, type: :controller do
+  describe 'GET #index' do
+    it "populates an array of tweets ordered by created_at DESC" do
+    end
+
+    it "renders the :index template" do
+    end
+  end
+end
