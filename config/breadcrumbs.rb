f0d5e4afb9d9ee704518a96e4883fb3ed5c38407
Sota Takahashi <sotq17@gmail.com>
2019-04-22 19:06:49 +0900

    コントローラー更新
    コントローラー更新

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,13 +2,21 @@ crumb :root do
   link "メルカリ", root_path
 end
 
-
+#カテゴリー
 crumb :categories do
   link 'カテゴリー一覧', categories_path
+  parent :root
 end
 
-
-crumb :category do |category|
-  link "#{category.name}",category_path(category)
+crumb :show_categories do |category|
+  link "#{category.name}", category_path
   parent :categories
 end
+
+#アイテム
+
+crumb :show_items do |item|
+  link "#{item.name}", item_path
+  parent :root
+end
+
