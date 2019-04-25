be63d024779fe23103bf525a50b18e8d9b496bb5
yoshimasa1125 <yoshimasahosoi1125@gmail.com>
2019-04-25 21:34:39 +0900

    ユーザー情報の保存

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-
+  before_action :configure_permitted_parameters
 	def new
 	end
 
@@ -10,11 +10,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
      @user = User.new(create_params)
      @user.save
-     redirect_to root_path
+  end
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:registrations, keys: [:email, :password, :nickname, :last_name, :first_name, :first_kana, :last_kana, :birth_year_id, :birth_month_id, :birth_day_id, :postal_code, :region_id, :city, :adress, :phone, :building, :card_number, :security_code, :expiration_year_id, :expiration_month_id])
   end
 
   private
   def create_params
-      params.require(:user).permit(:password, :nickname, :email, :last_name, :first_name, :first_kana, :last_kana, :birth_year_id, :birth_month_id, :birth_day_id, :region_id)
+      params.require(:user).permit(:email, :password, :nickname, :last_name, :first_name, :first_kana, :last_kana, :birth_year_id, :birth_month_id, :birth_day_id, :postal_code, :region_id, :city, :adress, :phone, :building, :card_number, :security_code, :expiration_year_id, :expiration_month_id)
   end
 end
