2ebc1a9d6ff5e9bb423a12a8e37af50d792ef8e4
Sota Takahashi <sotq17@gmail.com>
2019-04-29 19:17:50 +0900

    パンくず追加
    パンくず追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -8,9 +8,37 @@ crumb :categories do
   parent :root
 end
 
-crumb :show_categories do |category|
-  link "#{category.name}", category_path
+#レディース等の大カテゴリが選択された場合
+crumb :show_categories1 do
+  link "#{Category.find_by(id: params[:id]).name}", category_path
   parent :categories
+end
+
+# トップス等の中カテゴリが選択された場合
+crumb :show_categories_parent1 do
+  main_category_id = Category.find_by(id: params[:id]).parent
+  link "#{Category.find_by(id: main_category_id).name}",category_path(id: main_category_id)
+  parent :categories
+end
+  crumb :show_categories2 do
+  link "#{Category.find_by(id: params[:id]).name}"
+  parent :show_categories_parent1
+end
+
+# Tシャツ等の小カテゴリが選択された場合
+crumb :show_categories_parent do
+  main_category_id = Category.find_by(id: params[:id]).parent.parent
+  link "#{Category.find_by(id: main_category_id).name}",category_path(id: main_category_id)
+  parent :categories
+end
+crumb :show_categories_child do
+  sub_category_id = Category.find_by(id: params[:id]).parent
+  link "#{Category.find_by(id: sub_category_id).name}",category_path(id: sub_category_id)
+  parent :show_categories_parent
+end
+crumb :show_categories3 do
+  link "#{Category.find_by(id: params[:id]).name}"
+  parent :show_categories_child
 end
 
 #アイテム
@@ -20,3 +48,10 @@ crumb :show_items do |item|
   parent :root
 end
 
+
+# ユーザー
+
+crumb :show_users do |user|
+  link "マイページ", user_path
+  parent :root
+end
